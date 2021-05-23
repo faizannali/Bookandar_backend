f3589db03d5b1be460a34f8b4f1d49dbd06cb6e3
Faizan Ali <faizan.fa76@gmail.com>
2021-05-23 20:32:19 +0530

    23/5/21

--- API/src/services/app.js
+++ API/src/services/app.js
@@ -28,14 +28,13 @@ app.post('/login/userCheck', (req, res) => {
         console.log('connected as id ' + connection.threadId);
         const body = JSON.parse(JSON.stringify(req.body));
         params = [body.phoneNumber, body.password];
-        connection.query('SELECT phoneNumber,password from userinfo where phoneNumber = ? and password = ?', params, (err, data) => {
+        connection.query('SELECT fullName,phoneNumber,password from userinfo where phoneNumber = ? and password = ?', params, (err, data) => {
             connection.release() // return the connection to pool
             if (data.length == 0) {
-                res.send(JSON.parse('{"message":"user not found"}'));
-                console.log("hello");
+                res.send({ "fullName": "User Not Found" })
             }
             else if (data) {
-                res.send(JSON.parse('{"message":"user found"}'));
+                res.send(data[0]);
             }
         })
     })
@@ -51,34 +50,34 @@ app.post('/signUp', (req, res) => {
         connection.query('INSERT INTO userinfo SET ?', params, (err, rows) => {
             connection.release() // return the connection to pool
             if (!err) {
-                res.send({ "message": "User registered successfully." })
+                res.send({ "message": "User registered successfully" });
             } else {
                 // console.log(err)
-                res.send(err)
+                res.send({ "message": err });
             }
         })
     })
 });
 
 
-app.get('/allUsers', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if (err) console.log(err);
-        console.log('connected as id ' + connection.threadId);
-        // const body = JSON.parse(JSON.stringify(req.body));
-        // params = [body.phoneNumber, body.password];
-        connection.query('SELECT * from userinfo', (err, data) => {
-            connection.release() // return the connection to pool
-            if (data.length == 0) {
-                res.send(JSON.parse('{"message":"user not found"}'));
-                console.log("hello");
-            }
-            else if (data) {
-                res.send(data);
-            }
-        })
-    })
-});
+// app.get('/allUsers', (req, res) => {
+//     pool.getConnection((err, connection) => {
+//         if (err) console.log(err);
+//         console.log('connected as id ' + connection.threadId);
+
+//         // params = [body.phoneNumber, body.password];
+//         connection.query('SELECT * from userinfo', (err, data) => {
+//             connection.release() // return the connection to pool
+//             if (data.length == 0) {
+//                 res.send(JSON.parse('{"message":"user not found"}'));
+//                 console.log("hello");
+//             }
+//             else if (data) {
+//                 res.send(data);
+//             }
+//         })
+//     })
+// });
 
 //Insert Book Details
 app.post('/postBookDetails', (req, res) => {
@@ -91,74 +90,10 @@ app.post('/postBookDetails', (req, res) => {
         connection.query('INSERT INTO bookinfo SET ?', params, (err, rows) => {
             connection.release() // return the connection to pool
             if (!err) {
-                res.send({ "message": "The data from table are:11 " });
+                res.send({ "message": "Book Added successfully" });
             } else {
                 // console.log(err)
-                res.send(err);
-            }
-        })
-    })
-});
-
-//update Book Details
-
-app.post('/books/updateBook', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if (err) throw err
-        console.log(req.body)
-        params = [req.body.bookName, req.body.bookDesc, req.body.bookLocation, req.body.bookArea, req.body.bookPrice, req.body.bookId]
-        console.log(params);
-        connection.query('UPDATE bookinfo SET bookName = ?, bookDesc= ?,bookLocation=?,bookArea=?,bookPrice=? WHERE bookId = ?;', params, (err, rows) => {
-            connection.release() // return the connection to pool
-            if (err) {
-                console.log("error: ", err);
-                result(null, err);
-                return;
-            }
-            else {
-                res.send({ "message": "Done", "rows": rows });
-            }
-        })
-    })
-});
-
-
-//fetch all book details
-app.get('/fetch/bookDetails', (req, res) => {
-    pool.getConnection((err, connection) => {
-        const params = req.body
-        if (err) throw err
-        console.log("----------------", params);
-        connection.query('SELECT * FROM bookinfo;', (err, data) => {
-            connection.release() // return the connection to pool
-            if (!err) {
-                res.send({ "message": "Done", "rows": data });
-            } else {
-                console.log(err)
-            }
-            console.log('The data from bookDetails table are: \n', data)
-        })
-    })
-});
-
-
-
-//update my profile
-app.post('/books/updateUserProfile', (req, res) => {
-    pool.getConnection((err, connection) => {
-        if (err) throw err
-        console.log(req.body)
-        params = [req.body.fullName, req.body.email, req.body.location, req.body.password, req.body.phoneNumber]
-        console.log(params);
-        connection.query('UPDATE userInfo SET fullName = ?, email= ?, location=?, password=? WHERE phoneNumber = ?;', params, (err, rows) => {
-            connection.release() // return the connection to pool
-            if (err) {
-                console.log("error: ", err);
-                result(null, err);
-                return;
-            }
-            else {
-                res.send({ "message": "Done", "rows": rows });
+                res.send({ "message": err });
             }
         })
     })
@@ -175,10 +110,30 @@ app.post('/postMyAds', (req, res) => {
         connection.query('INSERT INTO user_ads SET ?', params, (err, rows) => {
             connection.release() // return the connection to pool
             if (!err) {
-                res.send({ "message": "Data Inserted " });
+                res.send({ "message": "Ad Successfully Posted." });
             } else {
                 // console.log(err)
-                res.send({ "Error": err });
+                res.send({ "message": err });
+            }
+        })
+    })
+});
+
+//Show my ads
+app.post('/showMyAds', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err
+
+        const params = [req.body.phoneNumber];
+        console.log(params);
+        connection.query('Select * From user_ads Where phoneNumber = ?', params, (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.send({ "message": "Success", "info": rows });
+            } else {
+                // console.log(err)
+                res.send({ "message": err });
             }
         })
     })
@@ -190,18 +145,101 @@ app.post('/deleteMyAds', (req, res) => {
     pool.getConnection((err, connection) => {
         if (err) throw err
 
-        const params = [req.body.phoneNumber, req.body.bookName];
+        const params = [req.body.phoneNumber, req.body.orderId];
         console.log(params);
-        connection.query('Delete From user_ads Where phoneNumber = ? and bookName = ?', params, (err, rows) => {
+        connection.query('Delete From user_ads Where phoneNumber = ? and orderId = ?', params, (err, rows) => {
             connection.release() // return the connection to pool
             if (!err) {
-                res.send({ "message": "Data deleted ", "info": rows });
+                res.send({ "message": "Deleted Successfully", "info": rows });
             } else {
                 // console.log(err)
-                res.send({ "Error": err });
+                res.send({ "message": err });
             }
         })
     })
 });
+
+//update Book Details
+
+app.post('/updateMyAds', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err
+        console.log(req.body)
+        params = [req.body.bookName, req.body.bookDesc, req.body.bookGenre, req.body.bookLocation, req.body.bookArea, req.body.bookPrice, req.body.orderId]
+        console.log(params);
+        connection.query('UPDATE user_ads SET bookName = ?, bookDesc= ?,bookGenre= ?,bookLocation=?,bookArea=?,bookPrice=? WHERE orderId = ?;', params, (err, rows) => {
+            connection.release() // return the connection to pool
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+                res.send({ "message": "Failed" });
+                return;
+            }
+            else {
+                res.send({ "message": "Updated Successfully" });
+            }
+        })
+    })
+});
+
+
+//update my profile
+app.post('/books/updateUserProfile', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) throw err
+        console.log(req.body)
+        params = [req.body.email, req.body.area, req.body.phoneNumber]
+        console.log(params);
+        connection.query('UPDATE userInfo SET email= ?, area=? WHERE phoneNumber = ?;', params, (err, rows) => {
+            connection.release() // return the connection to pool
+            if (err) {
+                console.log("message: ", err);
+                result(null, err);
+                return;
+            }
+            else {
+                res.send({ "message": "Profile updated" });
+            }
+        })
+    })
+});
+
+app.post('/profileInfo', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err
+
+        const params = [req.body.phoneNumber];
+        console.log(params);
+        connection.query('Select * From userinfo Where phoneNumber = ?', params, (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.send(rows[0]);
+            } else {
+                // console.log(err)
+                res.send({ "message": err });
+            }
+        })
+    })
+});
+
+app.post('/allAds', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if (err) throw err
+        connection.query('Select * From user_ads', (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.send({ "message": "Success", "info": rows });
+            } else {
+                // console.log(err)
+                res.send({ "message": err });
+            }
+        })
+    })
+});
+
+
+
 // Listen on enviroment port or 5000
 app.listen(port, () => console.log(`Listening on port ${port}`))
